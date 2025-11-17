fac87caf2ed129474f7fbd58b15ca7b3d9629477
JAINDIKSHA14 <jaindiksha318@gmail.com>
2025-11-17 17:02:59 +0530

    code completed

---
+++ file.js
@@ -0,0 +1,20 @@
+const fs=require("fs");
+//sync
+//fs.writeFileSync("./test.txt","hello world");
+//async
+fs.writeFile("./test.txt","hello diksha!",(err)=>{});
+//sync
+//const result=fs.readFileSync("./test.txt","utf-8");
+//console.log(result);
+//async
+/*fs.readFile("./test.txt","Utf-8",(err,res)=>{
+    if(res){
+        console.log(res);
+    }
+        else{
+            console.log(" result not found");  
+        }
+    }
+)*/
+fs.appendFile("./test.txt","what happened?",(err)=>{});
+fs.cpSync("./test.txt","./copy.txt")
---
+++ hello.js
@@ -0,0 +1,2 @@
+const math= require("./maths");
+console.log( "the sum is:", math(2,3));
